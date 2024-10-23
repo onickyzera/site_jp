e361dea52e757db94ff834f17588021e771b2ea6
Nícolas <linky152@gmail.com>
2024-10-22 21:24:03 -0300

    novo site

--- scripts.js
+++ scripts.js
@@ -1,36 +1,17 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
-        });
-        });
-    });
-    
-    let currentIndex = 0;
-    const produtos = document.querySelectorAll('.produto');
-    const totalProdutos = produtos.length;
-    
-    document.querySelector('.next').addEventListener('click', () => {
-      changeSlide('next');
-    });
-    
-    document.querySelector('.prev').addEventListener('click', () => {
-      changeSlide('previous');
-    });
-    
-    function changeSlide(direction) {
-      produtos[currentIndex].classList.remove('active');
-    
-      if (direction === 'next') {
-        currentIndex = (currentIndex + 1) % totalProdutos; // Avança para o próximo produto
-      } else if (direction === 'previous') {
-        currentIndex = (currentIndex - 1 + totalProdutos) % totalProdutos; // Volta para o produto anterior
+// script.js
+
+// Rolagem suave com compensação da altura do menu fixo
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          e.preventDefault();
+          const navbarHeight = document.querySelector('.navbar').offsetHeight;
+          const elementPosition = target.offsetTop - navbarHeight;
+          window.scrollTo({
+              top: elementPosition,
+              behavior: 'smooth'
+          });
       }
-    
-      produtos[currentIndex].classList.add('active');
-    }
-    
-    // Inicializa o carrossel com o primeiro produto visível
-    produtos[currentIndex].classList.add('active');
+  });
+});
